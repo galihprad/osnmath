994b9da7dfb26ca38b7fa4b208da5d3ecfebc421
galihprad <galih.p.w.j@gmail.com>
2021-10-06 09:38:16 +0700

    Try

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,7 +39,7 @@ const Layout = ({ children }) => {
             marginTop: `2rem`,
           }}
         >
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Built withooo
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
